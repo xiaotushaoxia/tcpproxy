120a7cb0ff95058a88ae2eebd78921d75999ec96
tuzhiwei <tuzhiwei@iplusbot.com>
2022-12-12 11:51:05 +0800

    adj headle func

--- proxy.go
+++ proxy.go
@@ -47,16 +47,16 @@ func (p *Proxy) Run(ctx context.Context, wg *sync.WaitGroup) {
 		p.logger.Printf("listen err: %s", err)
 		return
 	}
-	var er error
+
 	exit := make(chan int) // 防止下面这个go泄露
+	defer close(exit)
 	go func() {
 		select {
 		case <-ctx.Done():
 			p.logger.Printf("ctx done, close listener")
-			er = listener.Close()
-			p.logger.Printf("listener close err:%s", formatErr(er))
 		case <-exit:
 		}
+		p.logger.Printf("listener close err:%s", formatErr(listener.Close()))
 	}()
 
 	connID := 0
@@ -69,8 +69,8 @@ func (p *Proxy) Run(ctx context.Context, wg *sync.WaitGroup) {
 				p.logger.Printf("ctx cancel, proxy exit")
 				return
 			default:
-				// fixme  啥情况下ctx没有done 但是accept出错? 咋处理?
-				close(exit)
+				// fixme  啥情况下ctx没有done(listener没close) 但是accept出错? 咋处理?
+				// fixme 到这里以后在跑的p.handle要不要都退出?
 				return
 			}
 		}
@@ -92,51 +92,48 @@ func (p *Proxy) handle(ctx context.Context, wg *sync.WaitGroup, clientID int, co
 	}
 	cl.Printf("ProxyConn RemoteAddr:%s, ProxyConn LocalAddr:%s", rconn.RemoteAddr(), rconn.LocalAddr())
 
-	var once1 sync.Once
-	var once2 sync.Once
 	// fixme 感觉这里的关闭不是很优雅
 	closeConn := func() {
-		once2.Do(func() {
-			conn.Close()
-		})
-		once1.Do(func() {
-			rconn.Close()
-		})
-	}
-
-	warpCopy := func(connID int, dst *net.TCPConn, src *net.TCPConn, connLogger *HeadLogger) {
-		defer closeConn()
-		wrote, er := io.Copy(io.MultiWriter(dst, connLogger), src)
-		if src.LocalAddr().String() == p.laddr.String() {
-			cl.Printf("Summary: copy to backend %d bytes, exit err:%s", wrote, formatErr(er))
-		} else {
-			cl.Printf("Summary: copy to client %d bytes, exit err:%s", wrote, formatErr(er))
-		}
+		conn.Close()
+		rconn.Close()
 	}
 
 	var wgc sync.WaitGroup
 	wgc.Add(2)
-	go func() {
-		defer wgc.Done()
-		warpCopy(clientID, conn, rconn, NewHeadLogger("send:", cl.Printf)) // 从远端收数据发给客户端
-	}()
-	go func() {
-		defer wgc.Done()
-		warpCopy(clientID, rconn, conn, NewHeadLogger("recv:", cl.Printf)) // 从客户端收数据发给远端
-	}()
+
+	warpCopy := func(connID int, dst *net.TCPConn, src *net.TCPConn, connLogger *HeadLogger) {
+		defer func() {
+			select {
+			case <-ctx.Done():
+				//到这里表示ctx结束 不需要再closeConn
+			default:
+				closeConn()
+			}
+			wgc.Done()
+		}()
+
+		wrote, er := io.Copy(io.MultiWriter(dst, connLogger), src)
+		end := "client"
+		if src.LocalAddr().String() == p.laddr.String() {
+			end = "backend"
+		}
+		cl.Printf("Summary: copy to %s %d bytes, exit err:%s", end, wrote, formatErr(er))
+	}
+
+	go warpCopy(clientID, conn, rconn, NewHeadLogger("send:", cl.Printf)) // 从远端收数据发给客户端
+	go warpCopy(clientID, rconn, conn, NewHeadLogger("recv:", cl.Printf)) // 从客户端收数据发给远端
 	// 这里send的意思是proxy发数据给client， recv的意思是从指proxy从client收到数据
 
 	exit := make(chan int) // 防止下面这个go泄露
+	defer close(exit)
 	go func() {
 		select {
 		case <-ctx.Done():
 			cl.Printf("ctx done, close conn")
 			closeConn()
-		case <-exit:
-			return
+		case <-exit: // 到这里表示warpCopy出错退出了 不需要再closeConn
 		}
 	}()
 	wgc.Wait()
-	close(exit)
 	cl.Printf("exit")
 }
